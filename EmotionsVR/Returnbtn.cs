00fcae9f7b61ea6f5dc1c71e3658510135bedff8
Tsukasa88 <36668874+Tsukasa88@users.noreply.github.com>
2018-08-24 08:39:08 +0100

    Add files via upload

---
+++ EmotionsVR/Returnbtn.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Returnbtn : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void Return()
+	{
+		#if UNITY_5_3 || UNITY_5_3_OR_NEWER
+		SceneManager.LoadScene ("Language");
+		#else
+		Application.LoadLevel ("Language");
+		#endif
+	}
+}
